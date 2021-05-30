8f56c29cd8342520f5512a8810efb006f70b8c24
GitHub Action <action@github.com>
2021-05-30 20:45:06 +0000

    Add new version for GitHub Pages

--- docs/_dist_/index.js
+++ docs/_dist_/index.js
@@ -1,6 +1,8 @@
 /* This file is needed for Snowpack.
 */
 
+import json from './version.json.proxy.js';
+
 // Import main Elm module
 import Elm from './elm/Main.js';
 
@@ -10,6 +12,7 @@ import './styles/main.css.proxy.js';
 // Initialize Elm
 var app = Elm.Main.init({
   node: document.querySelector('main'),
+  flags: json.version
 });
 
 // Port for better drag-and-drop support in Firefox
